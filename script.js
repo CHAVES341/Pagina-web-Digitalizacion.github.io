63fdb34d98f53b154f070555cbd0e7c833ed80c5
CHAVES341 <jmartinezchaves05@gmail.com>
2025-05-02 00:15:33 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('formulario-contacto').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Gracias por contactarnos. Te responderemos pronto.');
+    this.reset();
+});
